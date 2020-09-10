2d5d64113f5db1398efe3d03c81b915bf68785ce
Freddec <freddec.exileng@gmail.com>
2020-09-10 11:51:48 +0200

    dev

---
+++ server/urls.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+
+
+
+################################################################################
+urlpatterns = [
+	#---------------------------------------------------------------------------
+	path('',                RedirectView.as_view(url='/game/')),
+    #---------------------------------------------------------------------------
+    path('impersonate/',    include('impersonate.urls')),
+    path('game/',           include('web_game.urls')),
+    #---------------------------------------------------------------------------
+] \
++ static('/favicon.ico', document_root=settings.STATIC_ROOT + '/favicon.ico') \
++ static('/assets/', document_root=settings.STATIC_ROOT + '/assets') \
++ static('/scripts/', document_root=settings.STATIC_ROOT + '/scripts')
+################################################################################
